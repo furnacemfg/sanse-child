10b04b9e0f143b9bba5b8427282a24e5405763be
Bryan Richard <bryan@furnaceai.com>
2018-01-29 13:51:55 -0700

    Removed attribution links and added styling for <code> tag

--- footer.php
+++ footer.php
@@ -14,19 +14,15 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-	
+
 		<?php get_sidebar( 'footer' ); // Loads the sidebar-footer.php template. ?>
-	
+
 		<div class="site-info">
-			
+
 			<?php
 				get_template_part( 'menus/menu', 'social' ); // Loads the menus/menu-social.php template.
 			?>
-			
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'sanse' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'sanse' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'sanse' ), 'Sanse', '<a href="https://foxland.fi/" rel="designer">Sami Keijonen</a>' ); ?>
-		
+
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
